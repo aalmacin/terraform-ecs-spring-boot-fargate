655fcc6f33f09072cad48efee03fb621a3160f46
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-05-14 13:05:18 +0000

    Using template files to setup ecr and task definition

--- spaced-repetition.tf
+++ spaced-repetition.tf
@@ -1,3 +1,11 @@
+data "template_file" "containers" {
+  template = "${file("spacedRepetitionContainerDefinitions.json")}"
+
+  vars {
+    "springBootECR" = "${aws_ecr_repository.spacedRepetitionECR.repository_url}"
+    "springBootMysqlECR" = "${aws_ecr_repository.spacedRepetitionMysqlECR.repository_url}"
+  }
+}
 
 resource "aws_ecs_cluster" "spacedRepetitionECSCluster" {
   name = "SpacedRepetition"
@@ -7,7 +15,19 @@ resource "aws_ecr_repository" "spacedRepetitionECR" {
   name = "spaced-repetition-spring-boot"
 }
 
+resource "aws_ecr_repository" "spacedRepetitionMysqlECR" {
+  name = "spaced-repetition-mysql"
+}
+
 resource "aws_ecs_task_definition" "spacedRepetitionTaskDefinition" {
-  family = "Spaced Repetition"
-  container_definitions = "${file("spacedRepetitionContainerDefinitions.json")}"
+  family = "SpacedRepetition"
+  container_definitions = "${data.template_file.containers.rendered}"
+}
+
+output "ecr-spring-boot" {
+  value = "${aws_ecr_repository.spacedRepetitionECR.repository_url}"
+}
+
+output "ecr-mysql" {
+  value = "${aws_ecr_repository.spacedRepetitionMysqlECR.repository_url}"
 }
