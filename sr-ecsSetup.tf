5cec6507be33052aab34f764973b960c41caab6c
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-05-15 12:54:07 +0000

    Converted setup to fargate

--- sr-ecsSetup.tf
+++ sr-ecsSetup.tf
@@ -17,7 +17,14 @@ resource "aws_ecs_service" "spacedRepetitionService" {
   desired_count = 1
   cluster = "${aws_ecs_cluster.spacedRepetitionECSCluster.arn}"
   load_balancer = []
-  network_configuration = []
+  network_configuration = [
+    {
+      subnets = [
+        "${aws_subnet.spacedRepetitionSubnet.id}",
+        "${aws_subnet.spacedRepetitionSubnet2.id}"
+      ]
+    }
+	]
 }
 
 resource "aws_ecr_repository" "spacedRepetitionECR" {
@@ -31,6 +38,11 @@ resource "aws_ecr_repository" "spacedRepetitionMysqlECR" {
 resource "aws_ecs_task_definition" "spacedRepetitionTaskDefinition" {
   family = "SpacedRepetition"
   container_definitions = "${data.template_file.containers.rendered}"
+  execution_role_arn = "${aws_iam_role.executionRole.arn}"
+  network_mode = "awsvpc"
+  cpu = 1024
+  memory = 2048
+  requires_compatibilities = ["FARGATE"]
 }
 
 output "ecr-spring-boot" {
