65327060d805947d9dd82330f7d80555c5079158
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-05-16 08:53:07 -0400

    Updated IAM and destroyed all services

--- sr-iam.tf
+++ sr-iam.tf
@@ -1,19 +1,42 @@
+
 resource "aws_iam_role_policy" "ecsExecutionRole" {
   name = "ecsExecution"
   role = "${aws_iam_role.executionRole.id}"
 
   policy = <<EOF
 {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+	"Version": "2012-10-17",
+	"Statement": [
+			{
+					"Effect": "Allow",
+					"Action": [
+							"ec2:AttachNetworkInterface",
+							"ec2:CreateNetworkInterface",
+							"ec2:CreateNetworkInterfacePermission",
+							"ec2:DeleteNetworkInterface",
+							"ec2:DeleteNetworkInterfacePermission",
+							"ec2:Describe*",
+							"ec2:DetachNetworkInterface",
+							"elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+							"elasticloadbalancing:DeregisterTargets",
+							"elasticloadbalancing:Describe*",
+							"elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+							"elasticloadbalancing:RegisterTargets",
+							"route53:ChangeResourceRecordSets",
+							"route53:CreateHealthCheck",
+							"route53:DeleteHealthCheck",
+							"route53:Get*",
+							"route53:List*",
+							"route53:UpdateHealthCheck",
+							"servicediscovery:DeregisterInstance",
+							"servicediscovery:Get*",
+							"servicediscovery:List*",
+							"servicediscovery:RegisterInstance",
+							"servicediscovery:UpdateInstanceCustomHealthStatus"
+					],
+					"Resource": "*"
+			}
+	]
 }
 EOF
 }
@@ -26,14 +49,14 @@ resource "aws_iam_role" "executionRole" {
   "Version": "2012-10-17",
   "Statement": [
     {
-      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
       "Principal": {
         "Service": "ecs.amazonaws.com"
       },
-      "Effect": "Allow",
-      "Sid": ""
+      "Action": "sts:AssumeRole"
     }
   ]
 }
 EOF
 }
+
