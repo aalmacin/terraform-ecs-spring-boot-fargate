610aa03b0e4bc5155790a369c65f741ac1f7dc7a
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-05-19 18:49:08 +0000

    Added policies

--- sr-ecsSetup.tf
+++ sr-ecsSetup.tf
@@ -15,7 +15,9 @@ resource "aws_ecs_service" "spacedRepetitionService" {
   name = "website"
   task_definition = "${aws_ecs_task_definition.spacedRepetitionTaskDefinition.arn}"
   desired_count = 1
+  health_check_grace_period_seconds=300
   cluster = "${aws_ecs_cluster.spacedRepetitionECSCluster.arn}"
+  iam_role = "${aws_iam_role.executionServiceRole.arn}"
 
   load_balancer = {
     target_group_arn = "${aws_lb_target_group.springBootContainer.arn}"
@@ -35,7 +37,8 @@ resource "aws_ecs_service" "spacedRepetitionService" {
 
   depends_on = [
     "aws_lb_listener.spacedRepetitionSiteHttpListener",
-    "aws_lb_listener.spacedRepetitionSiteHttpsListener"
+    "aws_lb_listener.spacedRepetitionSiteHttpsListener",
+    "aws_iam_role_policy.ecsInstanceRolePolicy"
   ]
 }
 
@@ -50,8 +53,8 @@ resource "aws_ecr_repository" "spacedRepetitionMysqlECR" {
 resource "aws_ecs_task_definition" "spacedRepetitionTaskDefinition" {
   family = "SpacedRepetition"
   container_definitions = "${data.template_file.containers.rendered}"
-  execution_role_arn = "${aws_iam_role.executionRole.arn}"
   network_mode = "awsvpc"
+  execution_role_arn = "${aws_iam_role.executionRole.arn}"
   cpu = 1024
   memory = 2048
   requires_compatibilities = ["FARGATE"]
--- sr-iam.tf
+++ sr-iam.tf
@@ -1,62 +1,31 @@
-
-resource "aws_iam_role_policy" "ecsExecutionRole" {
-  name = "ecsExecution"
-  role = "${aws_iam_role.executionRole.id}"
-
-  policy = <<EOF
-{
-	"Version": "2012-10-17",
-	"Statement": [
-			{
-					"Effect": "Allow",
-					"Action": [
-							"ec2:AttachNetworkInterface",
-							"ec2:CreateNetworkInterface",
-							"ec2:CreateNetworkInterfacePermission",
-							"ec2:DeleteNetworkInterface",
-							"ec2:DeleteNetworkInterfacePermission",
-							"ec2:Describe*",
-							"ec2:DetachNetworkInterface",
-							"elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-							"elasticloadbalancing:DeregisterTargets",
-							"elasticloadbalancing:Describe*",
-							"elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-							"elasticloadbalancing:RegisterTargets",
-							"route53:ChangeResourceRecordSets",
-							"route53:CreateHealthCheck",
-							"route53:DeleteHealthCheck",
-							"route53:Get*",
-							"route53:List*",
-							"route53:UpdateHealthCheck",
-							"servicediscovery:DeregisterInstance",
-							"servicediscovery:Get*",
-							"servicediscovery:List*",
-							"servicediscovery:RegisterInstance",
-							"servicediscovery:UpdateInstanceCustomHealthStatus"
-					],
-					"Resource": "*"
-			}
-	]
+data "template_file" "iamHostRolePolicy" {
+	template = "${file("./policies/iamHostRolePolicy.json")}"
 }
-EOF
+
+data "template_file" "iamServiceRolePolicy" {
+	template = "${file("./policies/iamServiceRolePolicy.json")}"
+}
+
+data "template_file" "iamInstanceRolePolicy" {
+	template = "${file("./policies/iamInstanceRolePolicy.json")}"
+}
+
+resource "aws_iam_role_policy" "ecsInstanceRolePolicy" {
+  name = "ecsExecutionPolicy"
+  role = "${aws_iam_role.executionServiceRole.id}"
+
+  policy = "${data.template_file.iamInstanceRolePolicy.rendered}"
 }
 
 resource "aws_iam_role" "executionRole" {
   name = "ecsExecutionRole"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+  assume_role_policy = "${data.template_file.iamHostRolePolicy.rendered}"
 }
-EOF
+
+resource "aws_iam_role" "executionServiceRole" {
+  name = "ecsExecutionServiceRole"
+
+  assume_role_policy = "${data.template_file.iamServiceRolePolicy.rendered}"
 }
 
